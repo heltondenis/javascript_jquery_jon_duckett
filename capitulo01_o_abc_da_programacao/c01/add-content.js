6c5abb75634674601b7ff58d160c07a2af98a265
Helton Denis <heltondenis.sistemas@gmail.com>
2018-04-04 22:59:40 -0300

    Adição de comentários

--- capitulo01_o_abc_da_programacao/c01/add-content.js
+++ capitulo01_o_abc_da_programacao/c01/add-content.js
@@ -1,7 +1,8 @@
-var today = new Date();
-var hourNow = today.getHours();
+var today = new Date(); // Cria um novo objeto date
+var hourNow = today.getHours(); // Verifica a data Atual
 var greeting;
 
+// Exibe uma mensagem de acordo com a hora atual
 if (hourNow > 18) {
   greeting = 'Noite';
 } else if (hourNow > 12) {
